f25fdead3e7f995e3c552da5b615aae72a3b228f
Cnnb01 <nyamambanyaguch@gmail.com>
2024-08-20 16:28:02 +0300

    webpack

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,4 +1,4 @@
-import $ from "jquery";
+const $ = require( "jquery" );
 $(document).ready(function(){
     $("body").append("<p>Holberton Dashboard</p>");
     $("body").append("<p>Dashboard data for the students</p>");
